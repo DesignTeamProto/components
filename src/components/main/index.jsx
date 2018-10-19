a1d09a7a4d729dc50c67780621783a15c7107dd6
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-10-19 15:33:51 +0200

    Added a main menu to the header component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -8,6 +8,11 @@ import MicroblogPost from '../MicroblogPost';
 import MicroblogPostExtended from '../MicroblogPostExtended';
 import MicroblogCreate from '../MicroblogCreate';
 import ImageGrid from '../ImageGrid';
+import TextFields from '../TextFields';
+import Select from '../Select';
+import Forms from '../Forms';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
 // const ContentLeft = styled.div`
 //   grid-column: col-start 1 / span 6;
@@ -17,21 +22,21 @@ class Main extends React.Component {
   render() {
     return (
       <Grid role="main" columns={12} gap={'2rem'}>
-          <Cell width={6}>
-            <MicroblogCreate />
-            <MicroblogPost />
-            <MicroblogPostExtended />
-          </Cell>
-          
-          <Cell width={6}>
-              {/* <ChromePicker
-              color={ this.state.background }
-              onChange={ this.handleChange }
-            /> */}
-            <ImageGrid />
-            <Card />
-            <Card />
-          </Cell>
+      <Cell width={6}>
+        <MicroblogCreate />
+        <MicroblogPost />
+        <MicroblogPostExtended />
+      </Cell>
+      
+      <Cell width={6}>
+        {/* <ChromePicker
+        color={ this.state.background }
+        onChange={ this.handleChange }
+      /> */}
+        <ImageGrid />
+        <Card />
+        <Card />
+      </Cell>
     </Grid>
     );
   }
